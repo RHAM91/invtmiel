ce488f556a92d943e34f1c088e37529396cccf2d
Romario Torres <cs360.x@hotmail.com>
2022-12-09 12:24:57 -0600

    Inventario
    Formulario para crear producto

--- src/components/Inventario/Productos/CrearProducto.vue
+++ src/components/Inventario/Productos/CrearProducto.vue
@@ -1,7 +1,33 @@
 <template>
-    <h1>
-        Formulario crear producto
-    </h1>
+    <b-container fluid="">
+        <form @submit.prevent="">
+            <b-row>
+                <b-col sm="12" class="mt-2">
+                <label for="">Código</label>
+                <b-form-input type="text" size="sm" v-model="codigo" required></b-form-input>
+                </b-col>
+
+                <b-col sm="12" class="mt-2">
+                <label for="">Descripción</label>
+                <b-form-input type="text" size="sm" v-model="descripcion" required></b-form-input>
+                </b-col>
+
+                <b-col sm="12" class="mt-2">
+                <label for="">Categoria</label>
+                <b-form-input type="text" size="sm" v-model="categoria" required></b-form-input>
+                </b-col>
+
+                <b-col sm="12" class="mt-2">
+                <label for="">Bodega</label>
+                <b-form-input type="text" size="sm" v-model="bodega" required></b-form-input>
+                </b-col>
+
+                <b-col sm="12" class="mt-2 d-flex flex-row-reverse">
+                <b-button type="submit" variant="success" size="sm">Guardar</b-button>
+                </b-col>
+            </b-row>
+        </form>
+    </b-container>
 </template>
 
 <script>
@@ -9,7 +35,10 @@ export default {
     name: 'CrearProducto',
     data() {
         return {
-            
+            codigo: '',
+            descripcion: '',
+            categoria: '',
+            bodega: ''
         }
     },
     methods: {
