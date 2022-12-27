0338c20a8994d95d32a77313db8e6462e402f10e
Romario Torres <cs360.x@hotmail.com>
2022-12-27 12:52:38 -0600

    Correcciones de tamaño de barra

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -82,12 +82,12 @@ export default {
     .top_verde_{
         background-color: #49C466;
         width: 100%;
-        height: 25px;
+        height: 35px;
         border-bottom: 1px solid #2ab74a;
     }
     .cuerpo_inicio_modulos{
         width: 100%;
-        height: calc(100% - 25px);
+        height: calc(100% - 35px);
         display: flex;
         justify-content: center;
         align-items: center;
